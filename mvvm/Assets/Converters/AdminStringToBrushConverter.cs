2104b2873b965fa840d79a5260cfd6359da76fb5
user_3_19P1 <user_3_19p1@mail.ru>
2023-05-19 12:49:32 +0500

    sdfsdf

---
+++ mvvm/Assets/Converters/AdminStringToBrushConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace Pishi_Wash__Store.Assets.Converters
+{
+    class AdminStringToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as ICollection<Orderproduct>;
+            if (str == null) { return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#76e383")); }
+            else if (str.Any(a => a.ProductArticleNumberNavigation.ProductQuantityInStock > 3)) { return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#20b2aa")); }
+            else { return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ff8c00")); }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
